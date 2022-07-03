d023d3ea393dcf8acdb93449686af13a03a4fa74
Emon Chowdhury <emonchowdhury494@gmail.com>
2022-07-03 14:04:54 +0600

    working to setup for one to many relationship

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function(){
+
+    Route::controller(CategoryController::class)->group(function () {
+        Route::get('category', 'index')->name('category.list');
+        // Route::get('/students/create', 'create')->name('students.create');
+        // Route::post('/students', 'store')->name('students.store');
+        // Route::get('/students/edit/{students}', 'edit')->name('student.edit');
+        //  Route::post('/students/{students}', 'update')->name('students.update');
+        //  Route::get('/students/{students}', 'destroy')->name('students.delete');
+        
+    });
+    
 });
